baf005ccb26ea81beadb67b684ad105c1f917fbf
xiaoyuan <seyooe@gmail.com>
2026-06-30 23:18:29 +0800

    Remove debugSwift from packages.

--- Examples/CodeAgent/CodeAgent/CodeAgentIOS/CodeAgentRootViewIOS.swift
+++ Examples/CodeAgent/CodeAgent/CodeAgentIOS/CodeAgentRootViewIOS.swift
@@ -9,25 +9,15 @@
 
 import SwiftUI
 import AgentKit
-#if DEBUG
-import DebugSwift
-#endif
 
 
 struct CodeAgentRootView: View {
     @Environment(\.scenePhase) private var scenePhase
     let agentRuntime = AgentRuntime.shared
     @Environment(AppContainer.self) private var container
-#if DEBUG
-    let debugSwift = DebugSwift()
-#endif
     
     init() {
-#if DEBUG
-        debugSwift
-            .setup(enableBetaFeatures: [.swiftUIRenderTracking])
-            .show()
-#endif
+
     }
 
     var body: some View {
